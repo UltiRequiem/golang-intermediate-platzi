bc39170a2b6d12d1a888418e537d6b18d2b746ce
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-09-18 15:19:04 -0500

    :recycle: Variadicas

--- src/variadicas/root.go
+++ src/variadicas/root.go
@@ -11,8 +11,42 @@ func sum(numbers ...int) int {
 	return total
 }
 
+func areFriends(names ...string) {
+	var message string
+	var totalFriends int = len(names)
+
+	switch totalFriends {
+
+	case 1:
+		message = fmt.Sprintf("%s has no friends.", names[0])
+	case 2:
+		message = fmt.Sprintf("%s and %s are friends.", names[0], names[1])
+	default:
+		for index, name := range names {
+			switch index {
+			case totalFriends - 1:
+				message += fmt.Sprintf("and %s", name)
+			case totalFriends - 2:
+				message += fmt.Sprintf("%s ", name)
+			default:
+				message += fmt.Sprintf("%s, ", name)
+
+			}
+
+		}
+		message = fmt.Sprintf("%s are friends.", message)
+	}
+
+	fmt.Println(message)
+}
+
 func main() {
-	// Both Valid
+	// All Valid
 	fmt.Println(sum(1, 2, 3, 4, 5))
 	fmt.Println(sum(1, 2, 5))
+	fmt.Println(sum(1))
+
+	areFriends("Zero", "Pedro", "Juanita")
+	areFriends("Suzaku", "Golem")
+	areFriends("Rockman")
 }
